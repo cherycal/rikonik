5e5bc21bb22c36f8afcd974f3a71907fb8b216a3
cherycal <chance_st@yahoo.com>
2026-05-09 01:08:52 -0700

    removed double quotes from sql in queryAPI

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -247,7 +247,7 @@ exports.queryAPI = async (req, res) => {
         // Final SQL — schema‑aware
         const sql = `
             SELECT ${cols}
-            FROM ${schema}."${table}"
+            FROM ${schema}.${table}
             ${queryWhere}
             ${orderTerm}
             LIMIT 2000
