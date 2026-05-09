7e13d9f1d241346f366d26a5adc6c37f396979e4
cherycal <chance_st@yahoo.com>
2026-05-09 11:38:48 -0700

    fixed routes in user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,33 +3,38 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const { queryTable } = require('../controllers/userController');
 
+// POST
+router.post('/', userController.queryTable);
 
-router.post('/',userController.queryTable);
-router.get('/',userController.default);
-router.get('/multi',userController.multi);
-router.get('/dbselect/:db',userController.dbselect);
-router.get('/api/query',userController.queryAPI);
-router.get('/table/:table',userController.selectTable);
-router.get('/queryTable/:table',userController.queryTable);
-// MOST specific first
+// BASIC ROUTES
+router.get('/', userController.default);
+router.get('/multi', userController.multi);
+router.get('/dbselect/:db', userController.dbselect);
+router.get('/api/query', userController.queryAPI);
+router.get('/table/:table', userController.selectTable);
+router.get('/queryTable/:table', userController.queryTable);
+
+// MOST SPECIFIC FIRST
 router.get('/query/:table/where/*/orderBy/:orderBy/ad/:ad', userController.queryTable);
 router.get('/query/:table/where/*/orderBy/:orderBy', userController.queryTable);
 router.get('/query/:table/where/*', userController.queryTable);
 
-// LESS specific
+// LESS SPECIFIC
 router.get('/query/:table/orderBy/:orderBy/ad/:ad', queryTable);
 router.get('/query/:table/orderBy/:orderBy', queryTable);
 
-// LEAST specific
+// LEAST SPECIFIC
 router.get('/query/:table', queryTable);
-router.get('/showTables',userController.showTables);
-router.get('*', (req,res) => {
+
+// SHOW TABLES (you had this twice)
+router.get('/showTables', userController.showTables);
+
+// ⭐ NEW: TABLE-ONLY ROUTE FOR SQUARESPACE
+router.get('/tableonly/:db?', userController.tableOnly);
+
+// WILDCARD MUST BE LAST
+router.get('*', (req, res) => {
     res.send("Page not found");
-})
-
-
-router.get('/showTables',userController.showTables);
+});
 
 module.exports = router;
-
-
