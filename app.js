5641e4ba80543bd0126339f61343131da5a05534
cherycal <chance_st@yahoo.com>
2026-05-09 11:08:26 -0700

    installed cors to app.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 const express = require("express");
 const path = require("path");
 
+const cors = require("cors");
+
+app.use(cors({
+    origin: "https://www.rikonik.com"
+}));
+
 
 const dotenv = require("dotenv");
 dotenv.config();
